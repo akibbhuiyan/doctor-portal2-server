37b576049eec72d776f360c65410f6a3a17a853c
akibbhuiyan <akibbhh@gmail.com>
2023-02-03 19:16:30 +0600

    XQC

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static("doctors"));
 app.use(fileUpload());
 const port = 5000;
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Doctor Portal server");
 });
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster1.g0q0cnp.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,85 +25,84 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-  try { 
-  const AppointmentCollection = client
-    .db("doctorPortal")
-    .collection("appointment");
-  const doctorsCollection = client.db("doctorPortal").collection("doctors");
+  try {
+    const AppointmentCollection = client
+      .db("doctorPortal")
+      .collection("appointment");
+    const doctorsCollection = client.db("doctorPortal").collection("doctors");
 
-  app.post("/addAppoinment", (req, res) => {
-    const appointment = req.body;
-    AppointmentCollection.insertOne(appointment).then((result) => {
-      res.send(result.acknowledged);
+    app.post("/addAppoinment", (req, res) => {
+      const appointment = req.body;
+      AppointmentCollection.insertOne(appointment).then((result) => {
+        res.send(result.acknowledged);
+      });
     });
-  });
-  app.post("/appoinmentsByDate", (req, res) => {
-    const date = req.body;
-    console.log(date);
-    const email = req.body.email;
-    const filters = { date: date.date };
+    app.post("/appoinmentsByDate", (req, res) => {
+      const date = req.body;
+      console.log(date);
+      const email = req.body.email;
+      const filters = { date: date.date };
 
-    doctorsCollection.find({ email: email }).toArray((err, doctors) => {
-      if (doctors.length === 0) {
-        filters.email = email;
-      }
-      AppointmentCollection.find(filters).toArray((err, document) => {
+      doctorsCollection.find({ email: email }).toArray((err, doctors) => {
+        if (doctors.length === 0) {
+          filters.email = email;
+        }
+        AppointmentCollection.find(filters).toArray((err, document) => {
+          res.send(document);
+        });
+      });
+    });
+    app.get("/appointments", (req, res) => {
+      AppointmentCollection.find({}).toArray((err, document) => {
         res.send(document);
       });
     });
-  });
-  app.get("/appointments", (req, res) => {
-    AppointmentCollection.find({}).toArray((err, document) => {
-      res.send(document);
-    });
-  });
-  app.post("/addDoctor", (req, res) => {
-    const file = req.files.file;
-    const name = req.body.name;
-    const email = req.body.email;
-    const number = req.body.number;
-    const filePath = `${__dirname}/doctors/${file.name}`;
+    app.post("/addDoctor", (req, res) => {
+      const file = req.files.file;
+      const name = req.body.name;
+      const email = req.body.email;
+      const number = req.body.number;
+      const filePath = `${__dirname}/doctors/${file.name}`;
 
-    file.mv(filePath, (err) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send({ msg: "File Upload Failed" });
-      }
-      return res.send({ name: file.name, path: `/${file.path}` });
-    });
-    const newImg = fs.readFileSync(filePath);
-    const encImg = newImg.toString("base64");
-    const image = {
-      contentType: req.files.file.mimetype,
-      size: req.files.file.size,
-      img: Buffer.from(encImg, "base64"),
-    };
-    doctorsCollection
-      .insertOne({ name: name, email: email, number: number, image })
-      .then((result) => {
-        fs.remove(filePath, (error) => {
-          if (error) {
-            console.log(error);
-            res.status(500).send({ msg: "File Upload Failed" });
-          }
-          res.send(result.acknowledged);
-        });
+      file.mv(filePath, (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send({ msg: "File Upload Failed" });
+        }
+        return res.send({ name: file.name, path: `/${file.path}` });
       });
-  });
-  app.get("/doctor", (req, res) => {
-    doctorsCollection.find({}).toArray((err, document) => {
-      res.send(document);
+      const newImg = fs.readFileSync(filePath);
+      const encImg = newImg.toString("base64");
+      const image = {
+        contentType: req.files.file.mimetype,
+        size: req.files.file.size,
+        img: Buffer.from(encImg, "base64"),
+      };
+      doctorsCollection
+        .insertOne({ name: name, email: email, number: number, image })
+        .then((result) => {
+          fs.remove(filePath, (error) => {
+            if (error) {
+              console.log(error);
+              res.status(500).send({ msg: "File Upload Failed" });
+            }
+            res.send(result.acknowledged);
+          });
+        });
     });
-  });
-  app.post("/isDoctor", (req, res) => {
-    const email = req.body.email;
-    doctorsCollection.find({ email: email }).toArray((err, doctors) => {
-      res.send(doctors.length > 0);
+    app.get("/doctor", (req, res) => {
+      doctorsCollection.find({}).toArray((err, document) => {
+        res.send(document);
+      });
     });
-  });
-}
-finally {
- }
+    app.post("/isDoctor", (req, res) => {
+      const email = req.body.email;
+      doctorsCollection.find({ email: email }).toArray((err, doctors) => {
+        res.send(doctors.length > 0);
+      });
+    });
+  } finally {
+  }
 }
 run().catch(console.log);
 
